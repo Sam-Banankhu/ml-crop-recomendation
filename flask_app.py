d7c15b9e19476e8df4e85968a925b6f3848561ba
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2023-02-22 01:21:45 +0200

    made updates

--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,36 @@
 import json
 import os
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from predictor import crop_predictor
+from predictor import Crop_predictor
 
 app = Flask(__name__)
 CORS(app)
-@app.route("/crop/",methods=['GET'])
-def return_price():
-  date = request.args.get('N')
-  month = request.args.get('P')
-  year = request.args.get('K')
-  crop = crop_predictor.predict(date, month, year) 
-  price_dict = {
-                'model':'model',
-                'crop': crop,
-                }
-  return jsonify(price_dict)
 
-@app.route("/",methods=['GET'])
+@app.route("/crop", methods=['POST'])
+def return_crop():
+    if request.content_type == 'application/json':
+        npk = request.get_json()
+    elif request.content_type == 'application/x-www-form-urlencoded':
+        npk = request.form.to_dict()
+    else:
+        return jsonify({'error': 'Unsupported Content-Type'}), 400
+        
+    N = npk['N']
+    P = npk['P']
+    K = npk['K']
+
+    crop = Crop_predictor.predict(N, P, K)
+    crop_dict = {
+        'model': 'model',
+        'crop': crop,
+    }
+
+    return jsonify(crop_dict)
+
+@app.route("/", methods=['POST', 'GET'])
 def default():
-  return "<h1> Welcome to crop recommendation system <h1>"
+    return "<h1>Welcome to crop recommendation system</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- predictor.py
+++ predictor.py
@@ -1,18 +1,35 @@
 import pickle
 import pandas as pd
 import numpy as np
+import joblib
+import warnings
+
+# Ignore DeprecationWarning messages
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# Your code that produces warning messages
+warnings.filterwarnings("ignore")
 
 
-class crop_predictor():
-  def __init__(self):
-    pass
+class Crop_predictor():
+    def __init__(self):
+        pass
+    
+    
+    def deserialize(self):
+        # de-serialize mlp_nn.pkl file into an object called model using pickle
+        # with open('model_1.pkl', 'rb') as handle:
+        model = joblib.load('model_1.sav')
+        # print('model loaded')
+        return model
   
-  def deserialize(self):
-    # de-serialize mlp_nn.pkl file into an object called model using pickle
-    with open('model.sav', 'rb') as handle:
-      model = pickle.load(handle)
-      return model
-  
-  def predict(self, N, P, K):
-    model = self.deserialize()
-    return model.predict(np.array([[N, P, K]]))
+    def predict(self,  N,  P, K):
+        model = self.deserialize()
+        return model.predict(np.array([[N, P, K]]))
+
+
+
+
+    # N = request.args.get('N')
+    # P = request.args.get('P')
+    # K = request.args.get('K')
