bf9fec21e9351f2da370c8ee67309d7debb0d918
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2023-02-21 22:33:16 +0200

    added models and flask app

---
+++ predictor.py
@@ -0,0 +1,18 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+
+class npk_predictor():
+  def __init__(self):
+    pass
+  
+  def deserialize(self):
+    # de-serialize mlp_nn.pkl file into an object called model using pickle
+    with open('model.sav', 'rb') as handle:
+      model = pickle.load(handle)
+      return model
+  
+  def predict(self, N, P, K):
+    model = self.deserialize()
+    return model.predict(np.array([[N, P, K]]))
